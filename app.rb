1c9b073b8efe73ef849f66ba918cbde3b7ee6432
Brady <bsutt123@gmail.com>
2018-05-03 11:11:37 -0400

    temporary commit to be fixed later

--- app.rb
+++ app.rb
@@ -1,16 +1,21 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'sinatra/cors'
 require 'sqlite3'
 
 set :port, 3001
+set :allow_origin, "http://127.0.0.1:5500 http://localhost:3000"
+set :allow_methods, "GET"
+set :allow_headers, "access-control-allow-origin"
+set :expose_headers, "location,link"
 
-db = SQLite3::Database.new( "movies_database.db" )
+db = SQLite3::Database.new( "database.db" )
 
 
 get '/movies' do
   rows = db.execute <<-SQL
-       SELECT * FROM movies
-       WHERE UPPER(originalTitle) LIKE '%CAR%'
+       SELECT * FROM entries
+       WHERE UPPER(original_title) LIKE '%CAR%'
        LIMIT 40;
   SQL
   titles = []
@@ -25,9 +30,9 @@ get '/search' do
   query = params['query']
   if query
     rows = db.execute <<-SQL
-       SELECT * from movies
-       WHERE UPPER(originalTitle) LIKE '%#{query}%'
-       ORDER BY originalTitle
+       SELECT * from entries
+       WHERE UPPER(original_title) LIKE '%#{query}%'
+       ORDER BY ttconst
        LIMIT 40;
     SQL
     titles = []
--- create_database.sql
+++ create_database.sql
@@ -1,4 +1,14 @@
-CREATE TABLE movies (
-       id integer PRIMARY KEY,
-       original_name TEXT,
+CREATE TABLE entries (
+       ttconst TEXT,
+       title_type TEXT,
+       primary_title TEXT,
+       original_title TEXT,
+       is_adult TEXT,
+       start_year TEXT,
+       end_year TEXT,
+       runtime TEXT,
+       genres TEXT
+       );
 
+.mode tabs
+.import title.basics.tsv entries
